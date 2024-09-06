7353b8b7bbe12acea11048c87466c62bc2f55a50
Lasath Fernando <devel@lasath.org>
2024-09-05 20:45:40 -0500

    Initial commit

---
+++ client/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Sockets;
+
+Console.WriteLine("Hello, World!");
+
+// Connect to the unix socket at /tmp/test.sock
+using var client = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+client.Connect(new UnixDomainSocketEndPoint("/tmp/test.sock"));
+using var stream = new NetworkStream(client, ownsSocket: true);
+
+while (stream.CanRead)
+{
+    var e = Event.Parser.ParseDelimitedFrom(stream);
+
+    if (e.SessionStarted != null)
+    {
+        Console.WriteLine($"Session started: {e.SessionStarted.Name}");
+    }
+    else if (e.SessionEnded != null)
+    {
+        Console.WriteLine(
+            $"Session ended: {e.SessionEnded.Name} (success: {e.SessionEnded.Success})"
+        );
+    }
+    else
+    {
+        Console.WriteLine("Unknown event");
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Sockets;
+using Google.Protobuf;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Listen on a unix socket at /tmp/test.sock
+using var listener = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+listener.Bind(new UnixDomainSocketEndPoint("/tmp/test.sock"));
+listener.Listen();
+
+// Accept a connection
+using var client = await listener.AcceptAsync();
+using var stream = new NetworkStream(client, ownsSocket: true);
+
+while (stream.CanRead) {
+    var start = new Event() {
+        SessionStarted = new SessionStarted() {
+            Name = Guid.NewGuid().ToString()
+        }
+    };
+
+    start.WriteDelimitedTo(stream);
+
+    var end = new Event() {
+        SessionEnded = new SessionEnded() {
+            Name = start.SessionStarted.Name,
+            Success = Random.Shared.Next(0, 2) == 0
+        }
+    };
+
+    end.WriteDelimitedTo(stream);
+}
